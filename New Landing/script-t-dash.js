7ea207c8f8823698dad725735e92bcc1be8ad4e6
iamyourdeveloper <52364314+iamyourdeveloper@users.noreply.github.com>
2022-04-11 01:03:47 -0400

    Add files via upload

---
+++ New Landing/script-t-dash.js
@@ -0,0 +1,75 @@
+// Label/Selected Label Stylings Upon Game Topics 
+// below:
+
+document.getElementById("col-lab-0").style.color = "white";
+document.getElementById("col-lab-1").style.color = "white";
+document.getElementById("col-lab-2").style.color = "white";
+document.getElementById("col-lab-3").style.color = "white";
+document.getElementById("col-lab-4").style.color = "white";
+document.getElementById("col-lab-5").style.color = "white";
+document.getElementById("col-lab-6").style.color = "white";
+
+
+document.getElementById('group-0').addEventListener("click", function() {
+    document.getElementById('col-lab-0').style.color = "#000";
+    document.getElementById('col-lab-1').style.color = "#fff";
+    document.getElementById('col-lab-2').style.color = "#fff";
+    document.getElementById('col-lab-3').style.color = "#fff";
+    document.getElementById('col-lab-4').style.color = "#fff";
+    document.getElementById('col-lab-5').style.color = "#fff";
+    document.getElementById('col-lab-6').style.color = "#fff";
+  });
+document.getElementById('group-1').addEventListener("click", function() {
+    document.getElementById('col-lab-0').style.color = "#fff";
+    document.getElementById('col-lab-1').style.color = "#000";
+    document.getElementById('col-lab-2').style.color = "#fff";
+    document.getElementById('col-lab-3').style.color = "#fff";
+    document.getElementById('col-lab-4').style.color = "#fff";
+    document.getElementById('col-lab-5').style.color = "#fff";
+    document.getElementById('col-lab-6').style.color = "#fff";
+  });
+document.getElementById('group-2').addEventListener("click", function() {
+    document.getElementById('col-lab-0').style.color = "#fff";
+    document.getElementById('col-lab-1').style.color = "#fff";
+    document.getElementById('col-lab-2').style.color = "#000";
+    document.getElementById('col-lab-3').style.color = "#fff";
+    document.getElementById('col-lab-4').style.color = "#fff";
+    document.getElementById('col-lab-5').style.color = "#fff";
+    document.getElementById('col-lab-6').style.color = "#fff";
+  });
+document.getElementById('group-3').addEventListener("click", function() {
+    document.getElementById('col-lab-0').style.color = "#fff";
+    document.getElementById('col-lab-1').style.color = "#fff";
+    document.getElementById('col-lab-2').style.color = "#fff";
+    document.getElementById('col-lab-3').style.color = "#000";
+    document.getElementById('col-lab-4').style.color = "#fff";
+    document.getElementById('col-lab-5').style.color = "#fff";
+    document.getElementById('col-lab-6').style.color = "#fff";
+  });
+document.getElementById('group-4').addEventListener("click", function() {
+    document.getElementById('col-lab-0').style.color = "#fff";
+    document.getElementById('col-lab-1').style.color = "#fff";
+    document.getElementById('col-lab-2').style.color = "#fff";
+    document.getElementById('col-lab-3').style.color = "#fff";
+    document.getElementById('col-lab-4').style.color = "#000";
+    document.getElementById('col-lab-5').style.color = "#fff";
+    document.getElementById('col-lab-6').style.color = "#fff";
+  });
+document.getElementById('group-5').addEventListener("click", function() {
+    document.getElementById('col-lab-0').style.color = "#fff";
+    document.getElementById('col-lab-1').style.color = "#fff";
+    document.getElementById('col-lab-2').style.color = "#fff";
+    document.getElementById('col-lab-3').style.color = "#fff";
+    document.getElementById('col-lab-4').style.color = "#fff";
+    document.getElementById('col-lab-5').style.color = "#000";
+    document.getElementById('col-lab-6').style.color = "#fff";
+  });
+document.getElementById('group-6').addEventListener("click", function() {
+    document.getElementById('col-lab-0').style.color = "#fff";
+    document.getElementById('col-lab-1').style.color = "#fff";
+    document.getElementById('col-lab-2').style.color = "#fff";
+    document.getElementById('col-lab-3').style.color = "#fff";
+    document.getElementById('col-lab-4').style.color = "#fff";
+    document.getElementById('col-lab-5').style.color = "#fff";
+    document.getElementById('col-lab-6').style.color = "#000";
+  });
